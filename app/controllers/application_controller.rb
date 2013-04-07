0a84a677672f64dc7b44402b9f02a78d89a26b89
Steve Singer <steve@ssinger.info>
2013-04-07 12:42:12 -0400

    updating title

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   def view
+    @title = 'MapExplorer - Exploring Communities Through Maps'
     render
   end
   def about
+      @title = 'MapExplorer - Exploring Communities Through Maps'
   end
   def penlakemap
     render
